2c1a2010f3da9fbea25557cb9e5d4de4c781ccdf
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-01-21 16:00:37 +0530

    Update and rename README.md to 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr():
+  printf("enter the value a,b,c");
+  scanf("%d%d%D",&a,&b,&c);
+  c=(a<=b);
+  printf("c is positive");
+  printf("c is negative");
+  getch()
+  }
