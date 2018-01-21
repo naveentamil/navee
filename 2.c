7243d0c3e2acc624f2b3705221c8fcf554f182d9
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-01-21 16:21:17 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+ int a,b;
+ clrscr();
+ printf("enter the vales a");
+ scanf("%d%d",&a,&b);
+ b=a++;
+ printf("the number is even",b);
+ getch();
+ }
