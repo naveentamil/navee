74d79a854da1d0ca926210220233e500303ee2e2
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-01-21 16:01:13 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -5,7 +5,7 @@ void main()
   int a,b,c;
   clrscr():
   printf("enter the value a,b,c");
-  scanf("%d%d%D",&a,&b,&c);
+  scanf("%d%d%d",&a,&b,&c);
   c=(a<=b);
   printf("c is positive");
   printf("c is negative");
