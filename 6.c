0e20c4ceb400cd6c557980f03b5b107039c0f5a8
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-01-25 16:11:33 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int year;
+  printf("enter the year");
+  scanf("%d",year);
+  if(year%d==0)
+   {
+    if(year%d==0)
+    {
+      printf("leap year");
+      }
+    else
+     {
+       printf("not ieap year");
+       }
+     }
+      else
+      {
+        pritf(" leap year")
+        }
+        getch();
+        }
