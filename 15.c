547acc88f9d3f981d81aad15c9c18eb8177261e1
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-01-30 16:38:00 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+#include<conio.h>
+void main()
+{
+int a,b,c,i;
+printf("   Give the First number for the Range :   \n");
+scanf("%d",&a);
+printf("   Give the Final number for the Range :   \n");
+scanf("%d",&b);
+printf("\n The Even numbers between %d and %d are ",a,b); 
+for(i=a; i<=b; ++i)
+{
+c= i % 2;
+if(c== 0)
+printf("\n  %d",i);
+}
+return 0; 
+} 
