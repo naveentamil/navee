42e88a5e9af599e3346185524f3628d8ad940d98
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-01-30 16:24:16 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            printf("no");
+            break;
+        }
+    }
+    if(i==n)
+    {
+        printf("yes");
+     }
+    getch();
+    }
