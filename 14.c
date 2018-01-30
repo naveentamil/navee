340b8931e348830c74eec52a80884b67e8df6c16
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-01-30 16:32:03 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,x=0,y=6;
+clrscr()
+    for(int i=x+1;i<y;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d ",i);
+        }
+        getch();
+    }
