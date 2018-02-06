93a110f85e7c48af47ac2222032fea274b3830f8
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-02-06 16:55:59 +0530

    Create n1.c

---
+++ n1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,d,n,value,i,sum=0;
+printf("enter the terms:");
+scanf("%d",&n);
+printf("enter the intial term:");
+scanf("%d",&a);
+printf("Enter the difference element:");
+scanf("%d",&d);
+value=a;
+printf("AP series");
+for(i=0;i<n;i++)
+  {
+printf("%d", value);
+sum=sum+ value;
+value=value+d;
+  }
+printf("sum of AP series till %d terms is=%d",n,sum);
+getch();
+}
