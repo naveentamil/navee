69384dac1d5e8b2f527d950384ba74d1844abbfa
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-02-06 16:25:16 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int sum=0,i,n;
+        clrscr();
+        printf("\n Please Give The Value of N:  ");
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %d = %d",i,5*i);
+        }
+        
+        getch();
+}
