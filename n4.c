85734e93b018f60f096c8bd6e7316e188d05195b
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-02-08 16:28:16 +0530

    Create n4.c

---
+++ n4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[20],i,j,n,temp;
+clrscr();
+printf("Enter the size of an array:");
+scanf("%d",&n);
+printf("Enter the elements in an array:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("The sorted array is:");
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+getch();
+}
