7ca390a9389886c42845faaa555ae87f85638b6c
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-02-13 16:18:56 +0530

    Create n31.c

---
+++ n31.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+    int len;
+    char arry[30]="laptop is good";
+    clrscr();
+    
+    len=strlen(arry);
+    printf(" \n strin length=%d \n",len);
+    getch();
+}
