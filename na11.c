ac09e3f2c897e24c8e2c4ffa521bb24b98dfd3b3
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-02-20 15:54:09 +0530

    Create na11.c

---
+++ na11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+void main() 
+{
+	char a[50];
+	int i,count=0;
+	clrscr();
+	gets(a);
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0' &&  a[i]<='9')
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	getch();
+}
