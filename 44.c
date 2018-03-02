107c358cca7d1da4ab903e6522362e007e608fae
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-02 14:05:29 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+ #include<conio.h>
+    void main()
+    {
+    	int n=6;
+      clrscr();
+    	if(n=!0&&n<=11)
+    	{
+    		printf("1 to 10");
+    	}
+    	else
+    	{
+    		printf("not 1 to 10");
+    	}
+      getch();
+    }
