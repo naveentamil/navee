ecb7a76842e31567fd19e283090461a9abd5d7ae
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-15 16:18:36 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+	int n,m,a;
+	clrscr();
+	scanf("%d %d",&n,&m);
+	a=n+m;
+	if(a%2==0)
+	printf("even");
+	else
+	printf("odd");
+	getch();
+}
