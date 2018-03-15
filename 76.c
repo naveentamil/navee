6407d2c9859225e3b947e3f5ac63be70325a51e5
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-15 16:29:04 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i;
+	clrscr();
+	scanf("%d",&n);
+	if(n==0)
+	{
+		printf("enter valid input ");
+		
+	}
+	else if(n==1)
+	{
+		printf("1 is neither prime nor composite");
+	}
+	else
+	{
+		for(i=2;i<=n;i++)
+		{
+			if(n%i==0 & n!=i)
+			{
+				printf("not prime");
+				break;
+				
+			}
+			 if(n%i==0 & n==i)
+			 {
+			 	printf("prime");
+			 }
+			
+			
+		}
+		
+	
+	}
+	getch();
+}
