784c962ba0b42db6f8e57574af614dda1a267ebd
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-16 15:09:47 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,n;
+ scanf("%d%d%d",&a,&b,&c);
+ if(a<b)
+ {
+  if(a<n)
+  printf("yes");
+  }
+  else
+  {
+printf("no");
+}
+getch();
+}
