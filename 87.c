9572ab54f4990a78371295b8d3c3c4e1341882dc
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-20 16:04:22 +0530

    Create 87.c

---
+++ 87.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i;
+	clrscr();
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			printf("\n%d",i);
+		}
+	}
+	getch();
+}
