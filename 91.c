06d302e8805ad9506bf13f4ca63928c0344cd4c5
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-20 16:23:30 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+	int a[10],b[10],c[10];
+	int n,i,j;
+	clrscr();
+	printf("enter the number of ninja warriors");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d %d",&a[i],&b[i]);
+		c[i]=b[i]-a[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d",c[i]);
+	}
+     getch();
+}
