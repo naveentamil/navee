d77de5adeb09d29bc9812ec187946de299c886f4
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-20 16:16:49 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[10];
+	int x,i;
+    clrscr();
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		
+		x=(int)(a[i]);
+		if(x%2!=0)
+		{
+			printf("\n%c",a[i]);	
+		}
+	}
+	getch();
+}
+
