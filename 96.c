81032614a549352d4dfe70e7e3480430904c98e9
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-20 16:45:26 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[10];
+	int i,flag=0,j;
+	clrscr();
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		for(j=i+1;a[j]!=0;j++)
+		{
+			if(a[i]==a[j])
+			{
+				printf("no");
+				flag=1;
+				break;
+			}
+		}
+		if(flag==1)
+		break;
+	}
+
+if(flag==0)
+{
+	printf("yes");
+}
+	getch();
+}
