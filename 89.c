69b28a1558d51112aec8419224fa7ab7f6a6f0f0
naveentamil <35652710+naveentamil@users.noreply.github.com>
2018-03-20 16:13:34 +0530

    Create 89.c

---
+++ 89.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,m,o,i,p,flag=0;
+	clrscr();
+	
+	scanf("%d %d",&n,&m);
+	o=m*n;
+	for(i=1;i<=o/2;i++)
+	{
+		p=i*i;
+		if(p==o)
+		{
+			printf("yes");
+			flag=1;
+			break;
+		}
+		
+		
+	}
+	if(flag==0)
+	printf("no");
+    getch();
+}
+
